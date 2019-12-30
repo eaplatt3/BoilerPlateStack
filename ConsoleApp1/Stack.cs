c8c02e007396e8235626b849c6643a6313482dfa
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-12-30 18:09:21 -0500

    Add project files.

---
+++ ConsoleApp1/Stack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackBoilerPlate
+{
+    //Stack Class
+    class Stack
+    {
+        static readonly int MAX = 1000;
+
+    }
+}
