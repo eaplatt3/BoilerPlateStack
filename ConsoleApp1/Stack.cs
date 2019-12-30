939381a4a432be0ab1a53b33b69bd5c2ba422ccd
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-12-30 18:12:18 -0500

    init Commit

--- ConsoleApp1/Stack.cs
+++ ConsoleApp1/Stack.cs
@@ -9,7 +9,12 @@ namespace StackBoilerPlate
     //Stack Class
     class Stack
     {
+        //Static for Max Capacity of Stack
+        //"readonly" Makes MAX Unable to be Changed
         static readonly int MAX = 1000;
+
+        int top;
+        int[] stack = new int[MAX];
 
     }
 }
