8f3bb853e426ce3791dc20bc5d049b6314e4cc48
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-12-31 17:21:58 -0500

    Added More Methods

--- ConsoleApp1/Stack.cs
+++ ConsoleApp1/Stack.cs
@@ -31,17 +31,53 @@ namespace StackBoilerPlate
             top = -1;
         }
 
+        //Method to Push Data into the Stack
         bool Push(int data)
         {
+            //Checks if Stack is Full
             if(top >= MAX)
             {
                 Console.WriteLine("Stack Overflow");
                 return false;
             }
+
+            //If not full Push Data on to the top 
             else
             {
                 stack[++top] = data;
                 return true;
+            }
+        }
+
+        //Method to Remove or "Pop" top Element off Stack
+        int Pop()
+        {
+            //Check if Stack has Data
+            if(top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return 0;
+            }
+
+            //Removes Data from Top of the Stack
+            else
+            {
+                int value = stack[top--];
+                return value;
+            }
+        }
+
+        void Peek()
+        {
+            if(top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+
+            else
+            {
+                Console.WriteLine("The Top Element in the Stack is : {0}", stack[top]);
             }
         }
     }
